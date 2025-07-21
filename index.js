3c35db53189a30ff28825c2480a7ac6dd4dd880b
Max-M06 <cymaxciceron2@gmail.com>
2025-07-21 12:21:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
+// Petit serveur juste pour garder Render en vie
 app.get("/", (req, res) => {
-  res.send("Bot is running on Render!");
+  res.send("Telegram bot is running.");
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
-})
+  console.log(`✅ Server running on port ${PORT}`);
+});
+
+// Lancer ton bot
+require("./telegramBot");
