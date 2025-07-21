a0bd3123414aed3dbd7c4a717ca2dd0cbcb8b677
Max-M06 <cymaxciceron2@gmail.com>
2025-07-21 12:14:19 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running on Render!");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+})
